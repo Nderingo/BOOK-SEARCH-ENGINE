1cfc63f06e6a432569e23828b5287c8b9fc8552e
Emmanuel Lyimo <esal29t@gmail.com>
2022-04-30 13:57:16 -0700

    deploying the application

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017git/Book-Engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Book-Engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
